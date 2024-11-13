6fd6d148583b4fae978381f9c24a7d6b6fd3a068
Varalaskhmi <varabtech08@gmail.com>
2024-11-13 12:00:08 +0530

    episode-3-part1

--- App.js
+++ App.js
@@ -1,9 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-heading = React.createElement('h1', {
-    id: 'heading'
-}, 'Welcome Hello')
+// heading = React.createElement('h1', {
+//     id: 'heading'
+// }, 'Welcome Hello')
+
 root = ReactDOM.createRoot(document.getElementById('root'))
 
-root.render(heading) 
+const JSXheading = <h1 id='heading'> Welcome to raect</h1>;
+root.render(JSXheading); 
+
+
