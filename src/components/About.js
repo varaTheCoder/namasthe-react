b6542aa5c7c4f20d353e7d3df5d3699634b17ac8
Varalaskhmi <varabtech08@gmail.com>
2024-11-27 19:33:35 +0530

    component life cycle (constructor, render, Componentdidmount

--- src/components/About.js
+++ src/components/About.js
@@ -1,13 +1,50 @@
+import React from "react";
 import User from "./User";
 import UserClass from "./UserClass";
 
-const About = () => {
-  return(
-  <>
-    <div>This is About us page</div>
-    {/* <User name='varalaskhmi' location='Hyderabad' contact='varabtech08' /> */}
-    <UserClass name='varalaskhmi' location='Hyderabad' contact='varabtech08' />
-  </>
-  );
-};
+// const About = () => {
+//   return(
+//   <>
+//     <div>This is About us page</div>
+//     {/* <User name='varalaskhmi' location='Hyderabad' contact='varabtech08' /> */}
+//     <UserClass name='varalaskhmi' location='Hyderabad' contact='varabtech08' />
+//   </>
+//   );
+// };
+
+//Chaniging to class component
+class About extends React.Component {
+    constructor () {
+        super();
+        console.log('Parent Constructor')
+
+    }
+
+    //used for calling the API's like useffect in functional components
+    componentDidMount(){
+        console.log('Parent componentDidMount')
+    }
+    
+  render() {
+    console.log('Parent Render')
+    return (
+      <>
+        <div>This is About us page</div>
+        {/* <User name='varalaskhmi' location='Hyderabad' contact='varabtech08' /> */}
+        <UserClass
+          name="varalaskhmi1"
+          location="Hyderabad"
+          contact="varabtech08"
+        />
+
+        <UserClass
+          name="varalaskhmi2"
+          location="Hyderabad"
+          contact="varabtech08"
+        />
+      </>
+    );
+  }
+}
+
 export default About;
--- src/components/UserClass.js
+++ src/components/UserClass.js
@@ -3,14 +3,19 @@ import React from "react";
 class UserClass extends React.Component {
     constructor(props) {
         super(props)
-
+        console.log('Child  Constructor');
         this.state = {
             count : 0,
             count2 : 2
         }
     }
+
+    componentDidMount(){
+        console.log(this.props.name+' Child componentDidMount')
+    }
     
 render() {
+    console.log('Child Constructor')
      const {name, location, contact} = this.props; //Object destructring
     return ( 
         <div className="user-card">
