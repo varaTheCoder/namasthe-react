133290c79f611cbce2dc38f4f34024af930b5d14
Varalaskhmi <varabtech08@gmail.com>
2024-12-01 17:06:54 +0530

    grouping test cases with describe

--- src/components/__tests__/Contact.test.js
+++ src/components/__tests__/Contact.test.js
@@ -2,6 +2,8 @@ import Contact from "../Contact"
 import { render, screen } from "@testing-library/react"
 import '@testing-library/jest-dom';
 
+describe("Contact Page test cases", ()=> {
+    
 test('check component loads or not',() => {
     render(<Contact/> ) // render the component that want
      const heading = screen.getByRole("heading") // check the sp
@@ -26,7 +28,9 @@ test('check component loads or not',() => {
             test('Should load 2 input boxes inside my component',() => {
                 render(<Contact/> ) // render the component that want
                  const inputBoxes = screen.getAllByRole("textbox"); // 
-                expect(inputBoxes.length).toBe(3) // 
+                expect(inputBoxes.length).toBe(2) // 
                 })
     
 
+
+})
